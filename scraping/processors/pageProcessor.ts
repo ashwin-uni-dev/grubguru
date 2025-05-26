c1b21e717bad415100152cb62a8cce315bc17473
ashwin bose <ab4623@ic.ac.uk>
2025-05-26 11:11:15 +0100

    fix: closed headless browser tab after page processed

--- scraping/processors/pageProcessor.ts
+++ scraping/processors/pageProcessor.ts
@@ -8,7 +8,10 @@ export abstract class PageProcessor {
     async process(url: string): Promise<any[]> {
         try {
             const page = await this.scraper.getPage(url);
-            return await this.processStrategy(page);
+            const result = await this.processStrategy(page);
+
+            await page.close();
+            return result;
         } catch (e) {
             console.log(e);
             return Promise.resolve([]);
