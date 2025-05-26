37f833e094db1596470caeb0a24daf063ed4ba23
ashwin bose <ab4623@ic.ac.uk>
2025-05-26 09:22:57 +0100

    feat: implemented uber eats food item scraper

---
+++ scraping/foodScraper.ts
@@ -0,0 +1,18 @@
+import { CategoryProcessor, FoodItemProcessor, StoreProcessor } from "./processors";
+import { Scraper } from "./scraper";
+
+const LOCATION_URL = 'https://www.ubereats.com/gb/neighborhood/south-kensington-london-eng';
+const scraper = new Scraper();
+
+(async () => {
+  const storeProcessor = new StoreProcessor(scraper);
+  const foodItemProcessor = new FoodItemProcessor(scraper);
+
+  const stores = await storeProcessor.process(LOCATION_URL);
+  for (let storeUrl of stores) {
+    const foodItems = await foodItemProcessor.process(storeUrl);
+    console.log(foodItems);
+  }
+
+  await scraper.close();
+})();
---
+++ scraping/scraper.ts
@@ -0,0 +1,47 @@
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import randomUseragent from 'random-useragent';
+
+puppeteer.use(StealthPlugin());
+
+export class Scraper {
+    private browser: any;
+
+    static create() {
+        return new Scraper();
+    }
+
+    async setupBrowser(): Promise<void> {
+        const args = [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-gpu',
+            '--disable-dev-shm-usage',
+            '--disable-blink-features=AutomationControlled',
+        ];
+
+        this.browser = await puppeteer.launch({
+            headless: true,
+            args,
+            executablePath: '/usr/bin/chromium-browser'
+        });
+    }
+
+    async getPage(url: string) {
+        if (!this.browser) await this.setupBrowser();
+
+        const userAgent = randomUseragent.getRandom() || 'Mozilla/5.0';
+
+        const page = await this.browser.newPage();
+        await page.setUserAgent(userAgent);
+        await page.setViewport({ width: 1280, height: 800 });
+        
+        await page.goto(url, { waitUntil: 'networkidle2', timeout: 10000 });
+        
+        return page;
+    }
+
+    async close() {
+        await this.browser.close()
+    }
+}
