e88a85e0ebfe65330baed809fde2c28668ba1b0e
ashwin bose <ab4623@ic.ac.uk>
2025-05-29 18:47:16 +0100

    feat: added bottom menu

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Home, History, Settings } from 'lucide-react';
+
+const Footer = () => {
+    return (
+        <div className="fixed bottom-0 left-0 w-full bg-black text-white p-4 z-50">
+            <nav className="flex justify-around items-center h-full">
+                <button className="flex flex-col items-center text-sm">
+                    <Home size={12} strokeWidth={1} />
+                </button>
+
+                <button className="flex flex-col items-center text-sm">
+                    <History size={12} strokeWidth={1} />
+                </button>
+
+                <button className="flex flex-col items-center text-sm">
+                    <Settings size={12} strokeWidth={1} />
+                </button>
+            </nav>
+        </div>
+    );
+};
+
+export default Footer;
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import {ReactNode} from "react";
 import {useNavigate} from "react-router-dom";
+import Footer from "./Footer";
 
 const Layout = ({ back, children }: { back: boolean, children: ReactNode }) => {
     const navigate = useNavigate();
@@ -8,6 +9,7 @@ const Layout = ({ back, children }: { back: boolean, children: ReactNode }) => {
         <div className='flex flex-col lg:w-3/4 sm:w-full p-8'>
             { back && <p className='text-purple-500 underline' onClick={() => navigate(-1)}>Back</p> }
             { children }
+            <Footer />
         </div>
     )
 }
