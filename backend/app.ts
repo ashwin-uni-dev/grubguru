73848d3aa258733f2979c781496c51dd16ae424f
ashwin bose <ab4623@ic.ac.uk>
2025-06-05 11:24:33 +0100

    feat: removed same site requirement for session cookies

--- backend/app.ts
+++ backend/app.ts
@@ -26,7 +26,7 @@ app.use(
         cookie: {
             secure: Boolean(process.env.SECURE_COOKIE || 'false') ,
             httpOnly: true,
-            sameSite: 'lax',
+            sameSite: 'none',
         }
     })
 );
