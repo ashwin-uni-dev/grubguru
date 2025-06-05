ea23efd276b9c03cd966c2ee525e996e287571fc
ashwin bose <ab4623@ic.ac.uk>
2025-06-05 12:29:29 +0100

    fix: fixed redirect after entering budget

--- frontend/src/pages/Preset/Budget/index.tsx
+++ frontend/src/pages/Preset/Budget/index.tsx
@@ -2,9 +2,12 @@ import Layout from "../../../components/Layout";
 import React, { useState } from "react";
 import { usePreferences } from "../contexts/preferenceContext";
 import DonePreference from "../components/donePreference";
+import ProgressivePage from "../../../components/ProgressivePage";
+import {useNavigate} from "react-router-dom";
 
 const Budget = () => {
     const { addOrUpdatePreference } = usePreferences();
+    const navigate = useNavigate();
     const [budget, setBudget] = useState<number>(10);
 
     const minBudget = 0;
@@ -15,35 +18,40 @@ const Budget = () => {
         setBudget(Number(event.target.value));
     };
 
+    const handleDone = () => {
+        addOrUpdatePreference({ id: 'budget', value: budget });
+        navigate('../preferences');
+    }
+
     return (
         <Layout back={true}>
             <div className='flex flex-col h-full'>
-                <h1 className='text-2xl font-bold tracking-tighter text-gray-800'>What's your budget?</h1>
-                <div className='mt-6'>
-                    <p>
-                        £
+                <ProgressivePage title="What's your budget" action={handleDone} final={true}>
+                    <div className='mt-6'>
+                        <p>
+                            £
+                            <input
+                                type='number'
+                                min={minBudget}
+                                max={maxBudget}
+                                step={stepBudget}
+                                value={budget}
+                                onChange={handleBudgetChange}
+                                className='text-4xl font-semibold tracking-tighter inline-block border-b-2'
+                                style={{ width: `${String(budget).length * 20 + 20}px` }}
+                            />
+                        </p>
                         <input
-                            type='number'
+                            type='range'
                             min={minBudget}
                             max={maxBudget}
                             step={stepBudget}
                             value={budget}
                             onChange={handleBudgetChange}
-                            className='text-4xl font-semibold tracking-tighter inline-block border-b-2'
-                            style={{ width: `${String(budget).length * 20 + 20}px` }}
+                            className='w-full'
                         />
-                    </p>
-                    <input
-                        type='range'
-                        min={minBudget}
-                        max={maxBudget}
-                        step={stepBudget}
-                        value={budget}
-                        onChange={handleBudgetChange}
-                        className='w-full'
-                    />
-                </div>
-                <DonePreference id='budget' value={budget}/>
+                    </div>
+                </ProgressivePage>
             </div>
         </Layout>
     );
