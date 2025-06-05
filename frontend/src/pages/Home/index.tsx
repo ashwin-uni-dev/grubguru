5611eb4b36dcb965a7173d3000d33bfc1f26e427
ashwin bose <ab4623@ic.ac.uk>
2025-06-05 04:56:29 +0100

    feat: added food page with likes

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -46,14 +46,14 @@ const Home = () => {
                 {
                     foods.length ? foods.map((food: any, index) => (
                         <FoodCard food={food} />
-                    )) : <FoodCardSkeleton />
+                    )) : <div className='w-4/5'><FoodCardSkeleton /></div>
                 }
             </HorizontalSection>
             <HorizontalSection title='Your favourites'>
                 {
                     foods.length ? foods.map((food: any, index) => (
                         <FoodCard food={food} />
-                    )) : <FoodCardSkeleton />
+                    )) : <div className='w-4/5'><FoodCardSkeleton /></div>
                 }
             </HorizontalSection>
         </Layout>
