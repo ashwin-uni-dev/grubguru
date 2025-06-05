22c60b29d85cb0cd366b836f872e740331310020
ashwin bose <ab4623@ic.ac.uk>
2025-06-05 08:27:37 +0100

    feat: notification event handler listens for changes to user doc

--- backend/routes/events.ts
+++ backend/routes/events.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import { Request, Response } from "express";
 import {sseMiddleware} from "../middleware/sse";
+import {NotificationEventHandler} from "../events/NotificationEventHandler";
 
 const router = express.Router();
+const notificationHandler = NotificationEventHandler.create();
 
-router.get('/notifications/:userId', sseMiddleware, (req: Request, res: Response) => {
-    console.log('user subscribed to notifs')
-    setInterval(() => res.sseSend!({ time: new Date().toISOString() }), 1000);
-})
+router.get('/notifications/:userId', sseMiddleware, (req: Request, res: Response) => notificationHandler.watchUser(req, res));
 
 export default router;
