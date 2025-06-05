f4021644a70adc0f78e8e03816b55a4d77f0fc58
ashwin bose <ab4623@ic.ac.uk>
2025-06-05 11:21:30 +0100

    feat: setting secure cookie based on env

--- backend/app.ts
+++ backend/app.ts
@@ -24,7 +24,7 @@ app.use(
         resave: false,
         saveUninitialized: false,
         cookie: {
-            secure: false,
+            secure: Boolean(process.env.SECURE_COOKIE || 'false') ,
             httpOnly: true,
             sameSite: 'lax',
         }
